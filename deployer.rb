d7a8a2ce532b448854a65ecd0d034b9d1f6092d7
Steven Wilkin <stevenwilkin@gmail.com>
2015-07-07 17:37:37 +0100

    Make released_at human readable
    https://gist.github.com/aerickson/894068

--- deployer.rb
+++ deployer.rb
@@ -3,11 +3,23 @@ require 'sinatra/base'
 require_relative 'domain/app'
 
 class Deployer < Sinatra::Base
+  helpers do
+    def humanize(released_at)
+      secs = Time.now - DateTime.parse(released_at).to_time
+      [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
+        if secs > 0
+          secs, n = secs.divmod(count)
+          "#{n.to_i} #{name}"
+        end
+      }.compact.reverse.join(' ')
+    end
+  end
+
   get '/' do
     @apps = [
       App.new('Pension Guidance', 'fam5deeg2ob7das1yet8dyerr5ye4b', 'ggp-pension-guidance'),
       App.new('Output Document', 'output-pensionwise', 'staging-output-pensionwise'),
-      App.new('Locator', 'staging-locator-pensionwise', 'locator-pensionwise')
+      App.new('Locator', 'locator-pensionwise', 'staging-locator-pensionwise')
     ]
     erb :index
   end
