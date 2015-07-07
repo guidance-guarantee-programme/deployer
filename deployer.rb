f2e8ec108a28b435adc4c487eff058b8347afedc
Steven Wilkin <stevenwilkin@gmail.com>
2015-07-07 17:26:55 +0100

    Display table of apps and release info
    Will need Bootstrap'd

--- deployer.rb
+++ deployer.rb
@@ -1,7 +1,14 @@
 require 'sinatra/base'
 
+require_relative 'domain/app'
+
 class Deployer < Sinatra::Base
   get '/' do
+    @apps = [
+      App.new('Pension Guidance', 'fam5deeg2ob7das1yet8dyerr5ye4b', 'ggp-pension-guidance'),
+      App.new('Output Document', 'output-pensionwise', 'staging-output-pensionwise'),
+      App.new('Locator', 'staging-locator-pensionwise', 'locator-pensionwise')
+    ]
     erb :index
   end
 end
